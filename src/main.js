51b5fac30af372a9988cf1dcd5092d2c06cabaad
maomao <466597085@qq.com>
2020-03-04 11:06:18 +0800

    使用emlemnt和router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-04 09:26:38
+ * @LastEditTime: 2020-03-04 11:01:19
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \hmpygd:\前端文件\就业班\就业班\Vue进阶\11111\新建文件夹\hm-toutiao-pc-911\src\main.js
+ */
 import Vue from 'vue'
 import App from './App.vue'
-
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import router from './router'
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App)
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,16 @@
+/*
+ * @Author: your name
+ * @Date: 2020-03-04 10:58:47
+ * @LastEditTime: 2020-03-04 11:00:38
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \hmpygd:\前端文件\就业班\就业班\Vue进阶\11111\新建文件夹\hm-toutiao-pc-911\src\router\index.js
+ */
+
+import VueRouter from 'vue-router'
+import Vue from 'vue'
+Vue.use(VueRouter)
+const router = new VueRouter({
+  routes: []
+})
+export default router
